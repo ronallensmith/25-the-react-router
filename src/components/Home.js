5a33aa88cb8088757f47f731aa74bd093b56415b
ronallensmith <ron@ronallensmith.com>
2018-11-04 14:49:09 -0800

    first commit to GitHub

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home Page</h4>
+      <p>Home! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel explicabo, architecto delectus obcaecati cum soluta accusamus est dolorem maiores asperiores tempore repellat ea ratione modi voluptatum perspiciatis ut. Ipsam, vitae!</p>
+    </div>
+  )
+}
+
+export default Home;
